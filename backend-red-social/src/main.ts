1e7efdb0ac80b278b74890ba935c5b989ea1ca9b
ValentinBrazanovich <vvalente00@gmail.com>
2026-06-05 22:48:25 -0300

    corrección

--- backend-red-social/src/main.ts
+++ backend-red-social/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true, // permite que el frontend acceda a la API desde cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONES', // permite estos métodos HTTP
+    credentials: true, // permite enviar cookies y credenciales en las solicitudes
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
